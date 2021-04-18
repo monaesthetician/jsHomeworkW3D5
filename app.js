a1f34bac9f5c2d8b7ec81ffe7f45e0d8b3684ecc
Mona <emonearth915@gmail.com>
2021-04-18 16:36:17 -0400

    Javascript Arrow function

---
+++ app.js
@@ -0,0 +1,65 @@
+
+
+var a = 6
+var b = 3
+
+//  Addition
+function kingkong(a,b) {
+  return a + b; 
+};
+var result = kingkong(a,b);
+console.log(result);
+
+
+const godzila = (a, b)=> {
+  return a + b;
+};
+var result = godzila(a,b);
+console.log(result);
+
+
+//  subtraction
+
+function banana(a,b) {
+  return a - b; 
+};
+var result = banana(a,b);
+console.log(result);
+
+
+const organge = (a, b)=> {
+  return a - b;
+};
+var result = organge(a,b);
+console.log(result);
+
+// multiplication
+
+function coco(a,b) {
+  return a * b; 
+};
+var result = coco(a,b);
+console.log(result);
+
+
+const lilly = (a, b)=> {
+  return a * b;
+};
+var result = lilly(a,b);
+console.log(result);
+
+// division
+
+function cat(a,b) {
+  return a / b; 
+};
+var result = cat(a,b);
+console.log(result);
+
+
+const dog = (a, b)=> {
+  return a / b;
+};
+var result = dog(a,b);
+console.log(result);
+
